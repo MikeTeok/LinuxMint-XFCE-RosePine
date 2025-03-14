8d04daf69e7c1c3d226d27a918625953d8c5682a
MikeTeok <miketeok94@gmail.com>
2025-03-14 17:53:21 +0800

    first sync

---
+++ auto-sync.sh
@@ -0,0 +1,9 @@
+cp -r ~/.config/picom/ .config/
+cp -r ~/.config/gtk-3.0/ .config/
+cp -r ~/.config/eww/ .config/
+cp -r ~/.local/share/xfce4/terminal/colorschemes/ .local/share/xfce4/terminal/ 
+cp ~/.config/fish/config.fish .config/fish/
+cp -r ~/.config/fish/themes/ .config/fish/
+cp -r ~/.config/oh-my-posh/ .config/
+cp -r ~/.config/neofetch/ .config/
+cp ~/Documents/scripts/picom.sh auto-start-picom.sh
