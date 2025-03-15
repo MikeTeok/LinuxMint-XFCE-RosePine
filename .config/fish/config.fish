6e80fc682b44ef5485259aedb196f93d6ec2a5c7
MikeTeok <miketeok94@gmail.com>
2025-03-15 10:28:52 +0800

    update

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set fish_greeting
+fish_config theme choose "Rosé Pine Moon"
+oh-my-posh init fish --config ~/.config/oh-my-posh/easy-term.omp.json | source
+
