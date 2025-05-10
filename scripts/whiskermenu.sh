434ed96f7050e6258be7fae474a8598431e5e6f4
MikeTeok <miketeok94@gmail.com>
2025-05-10 10:27:56 +0800

    fix chatgpt css

--- scripts/whiskermenu.sh
+++ scripts/whiskermenu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xdotool getmouselocation | awk -F '[: ]+' '{print $2, $4}' | xargs -I {} sh -c 'xdotool mousemove 745 520 ; xfce4-popup-whiskermenu -p ; xdotool mousemove {}'
+xdotool getmouselocation | awk -F '[: ]+' '{print $2, $4}' | xargs -I {} sh -c 'xdotool mousemove 733 520 ; xfce4-popup-whiskermenu -p ; xdotool mousemove {}'
