d058e4fcb66d2585cee418bf64eb8e3a17375d43
MikeTeok <miketeok94@gmail.com>
2025-05-28 22:39:50 +0800

    regular sync

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,4 @@ set fish_greeting
 fish_config theme choose "Rosé Pine Moon"
 oh-my-posh init fish --config ~/.config/oh-my-posh/easy-term.omp.json | source
 source ~/.aliases
+export PATH="$HOME/Documents/StandaloneApp/nvim-linux-x86_64/bin:$PATH"
---
+++ scripts/nvim.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xfce4-terminal -e "/home/miketeok/Documents/StandaloneApp/nvim-linux-x86_64/bin/nvim \"$1\""
